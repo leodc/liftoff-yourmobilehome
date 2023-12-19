295586a16936c91c86fc25c519d51b6ed6cba24b
Leonel Castañeda <leodcastaneda@gmail.com>
2023-12-19 10:17:37 -0600

    make navbar responsive

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,13 +1,14 @@
 const NavigationBar = () => (
-  <nav className="z-[999] fixed h-12 w-full flex justify-end items-center px-20 backdrop-blur shadow drop-shadow">
-    <div className="flex gap-4 items-center text-white font-bold">
+  <nav 
+    className="z-[999] fixed h-12 w-full flex justify-end items-center backdrop-blur shadow drop-shadow px-2 md:px-20">
+    <div className="flex items-center text-white text-sm md:text-base gap-2 md:gap-4 font-semibold md:font-bold">
       <div>
         <p>
           Home
         </p>
       </div>
       <div>
-        <button className="bg-[#FF6600] py-2 px-4 rounded-full hover:bg-[#ff944d]">
+        <button className="bg-[#FF6600] rounded-full hover:bg-[#ff944d] py-1 md:py-2 px-3 md:px-4">
           Request an Offer
         </button>
       </div>
